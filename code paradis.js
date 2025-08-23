3b8f320063169042663f67db2fb189dcd78874aa
SuperWaifu <waifutablette@gmail.com>
2025-08-23 15:02:25 +0200

    Delete code paradis.js

--- code paradis.js
+++
@@ -1,8 +0,0 @@
-// Fonction pour démarrer le fondu et rediriger ensuite
-    function fadeOutAndRedirect(url) {
-        const body = document.querySelector('body');
-        body.classList.add('fade');
-        setTimeout(function () {
-            window.location.href = url;
-        }, 1000); // La durée ici doit correspondre à la durée de la transition CSS
-    }
